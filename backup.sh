65e6d3dfe5858582e39dde72e43c342d871a3f7e
Jeff Kilroy <jeffkilroy@gmail.com>
2015-12-14 10:11:16 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 TMP_DIR="/tmp/"
 DATE=$(date +"%Y-%m-%d_%H%M")
 BKP_FILE="$TMP_DIR/server_backup_$DATE.tar"
-BKP_DIRS="/var/www /etc/automysqlbackup"
+BKP_DIRS="/var/www /var/lib/automysqlbackup"
 DROPBOX_UPLOADER=/root/dropbox/dropbox_uploader.sh
 tar cf "$BKP_FILE" $BKP_DIRS
 gzip "$BKP_FILE"
